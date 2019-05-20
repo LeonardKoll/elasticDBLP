0fdb641efff9c93c8cf8f283496be9f682ad0478
UniversityPC <kcommunicate@outlook.com>
2019-05-20 19:22:01 +0200

    clean

--- dblpmigrate/xmlread.py
+++ dblpmigrate/xmlread.py
@@ -31,8 +31,7 @@ def worker(iterator, lock):
 
 url = 'http://localhost:9200/dblp/_doc'
 headers = {"Content-Type": "application/json"}
-pubTypes = (
-"data", "article", "inproceedings", "proceedings", "book", "incollection", "phdthesis", "mastersthesis", "www")
+pubTypes = ("data", "article", "inproceedings", "proceedings", "book", "incollection", "phdthesis", "mastersthesis", "www")
 
 count = 0;
 iterator = lxml.etree.iterparse("dblp.xml", load_dtd=True, tag=pubTypes);
@@ -46,27 +45,3 @@ wolfgang = threading.Thread(target=worker, args=(iterator, iteratorLock))
 wolfgang.start()
 jenny = threading.Thread(target=worker, args=(iterator, iteratorLock))
 jenny.start()
-
-
-
-"""
-for event, element in :
-
-    # API Call
-    pubData = {}
-    for child in element:
-        pubData[child.tag] = child.text
-    publication = {element.tag: pubData}
-    response = requests.post(url, data=json.dumps(pubData), headers=headers)
-    #print(response.text)
-
-    # Some out
-    count += 1
-    if count % 100 == 0:
-        print(count)
-
-    # Memory Mgmt
-    element.clear()
-    while element.getprevious() is not None:
-        del element.getparent()[0]
-"""
