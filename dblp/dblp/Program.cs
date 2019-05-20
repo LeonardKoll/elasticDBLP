2ecd56f0c13c6cb622352c46522e7df066042d88
UniversityPC <kcommunicate@outlook.com>
2019-05-20 14:03:24 +0200

    listen on 0.0.0.0 instead of localhost

--- dblp/dblp/Program.cs
+++ dblp/dblp/Program.cs
@@ -19,6 +19,7 @@ namespace dblp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:80")
                 .UseStartup<Startup>();
     }
 }
--- dblp/dblp/Startup.cs
+++ dblp/dblp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace dblp
 {
+    // dotnet publish -c Release --self-contained -r win-x64
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
